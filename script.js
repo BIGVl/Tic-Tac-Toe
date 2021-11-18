6cee1cd01a576350fb2064f23afdccd57393f600
bigVL <84589629+BIGVl@users.noreply.github.com>
2021-11-18 22:05:27 +0200

    Added the logic for the x and o to be pushed in the arrray at the right index/ Work in progress

--- script.js
+++ script.js
@@ -1,25 +1,36 @@
-//Creates and populates the gameboard
+//Creates and populates the gameboard when the player click on a cell
 const gameBoard = (()=>{
     let x = 'x';
     let o = 'o';
-    let i = 0;
+    let oTurn;
     
     const cells = document.querySelectorAll('.cell');
-    const markers = [x,o,x,o,x,o,x,o,x];
-
+    const markers = ['','','','','','','','',''];
+//Adds X or O to the clicked cell if the cell is empty
     cells.forEach(cell=>{
-        cell.classList.add(markers[i]);
-        i++
+        cell.addEventListener('click',(e)=>{
+            
+            currentClass = oTurn ? o : x;
+            cell.classList.add(currentClass);
+            oTurn = !oTurn;
+            markers.splice(e.target.dataset.index,1,currentClass)
+            checkWinner()
+        }, {once:true})
     })
+//checks for the winner and returns a screen declaring the winner
+    function checkWinner () {
+        if (markers[0]==='x' && markers[1]=='x' && markers[2]==='x') {
+            console.log('winner');
+        }
+    }
 
-    
+     return {markers}
 })();
 
 
 //Module that will display the menu 
 const displayController = (()=>{
 
+
 })();
 
-//Object to control the flow of the game
-//  const controllGame

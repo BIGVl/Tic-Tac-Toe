627880f01498a472e134b4699642ac2629ac5e15
bigVL <84589629+BIGVl@users.noreply.github.com>
2021-11-22 00:16:14 +0200

    Added the start menu and added functionalities for buttons start game and play again, there is a bug to fix and functionalities to add

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //Creates and populates the game when the player click on a cell
 const game = (()=>{
+//Important variables
     let x = 'x';
     let o = 'o';
-    let oTurn;
     const cells = document.querySelectorAll('.cell');
     const markers = ['','','','','','','','',''];
     let WINNING_COMBO = [
@@ -16,17 +16,23 @@ const game = (()=>{
         [2,4,6]
     ]
 //Adds X or O to the clicked cell if the cell is empty
+function playGame () {
+    let oTurn = false;
     cells.forEach(cell=>{
         cell.addEventListener('click',(e)=>{
             
              currentMarker = oTurn ? o : x;
+             if (cell.classList.contains('x') || cell.classList.contains('o') ) return;
             cell.classList.add(currentMarker);
+            console.log(oTurn);
             oTurn = !oTurn;
             markers.splice(e.target.dataset.index,1,currentMarker);
             checkWinner(currentMarker);
             endResetGame.endGame(false);
         }, {once:true})
     })  
+}
+playGame();
 //Checks for the winner every time a player clicks a cell
 function checkWinner (currentMarker) {
 
@@ -38,15 +44,46 @@ function checkWinner (currentMarker) {
 };
 
     
-return {checkWinner, markers}
+return {checkWinner, markers, cells, playGame}
     
 })();
 
+//Mdule that handles the start game menu and adds to the scoreboard or resets the scoreboard
+const setGame = (()=>{
+//Setting up the DOM
+    const player1 = document.querySelector('.ip1');
+    const player1Score = document.querySelector('.player1-score');
+    const player2 = document.querySelector('.ip2');
+    const player2Score = document.querySelector('.player2-score');
+    const startButton = document.querySelector('.start-game');
+    const overlayStart = document.querySelector('.overlay-Start');
 
-//Module that will display the menu 
-const endResetGame = (()=>{    
+    startButton.addEventListener('click', ()=>{
+        player1Score.textContent = player1.value ;
+        player2Score.textContent = player2.value ;
+        overlayStart.classList.add('out');
+        
 
-//Ends the round when a winner is declared so that none of the players can add anymore content to the board and declares the winner
+    })
+
+    
+    return {player1Score, player2Score}
+})();
+
+
+//Module that will handle the end of the game
+const endResetGame = (()=>{  
+    //Important variables  
+    const winnerScreen = document.querySelector('.winner-screen')
+    const scorep1 = document.querySelector('.score1');
+    const scorep2 = document.querySelector('.score2');
+    const winnerText = document.querySelector('.winner-text');
+    let score1track = 0;
+    let score2track = 0;
+    const playAgain = document.querySelector('.play-again');
+    const reset = document.querySelector('.reset');
+//Ends the round when a winner is declared so that none of the players can add anymore content to the board and displays the winner screen which contains 
+//the buttons reset or play again and declares the winner
 function endGame (check) {
     if (!game.checkWinner(currentMarker)) {
     check =  game.markers.every(mark=>{
@@ -54,19 +91,45 @@ function endGame (check) {
     });
 }
 if (check) {
-    console.log('mata')
+    winnerScreen.classList.add('active');
+    winnerText.textContent = 'It\'s a draw';
 }
 else if (game.checkWinner(currentMarker) && currentMarker==='x') {
-
-    console.log('x winner')
+    winnerScreen.classList.add('active');
+    winnerText.textContent = `${setGame.player1Score.textContent} has won the game`;
+    score1track++
+    scorep1.textContent = score1track;
 
 }
 else if (game.checkWinner(currentMarker) && currentMarker==='o') {
-    console.log('o winner')
+    winnerScreen.classList.add('active');
+    winnerText.textContent = `${setGame.player2Score.textContent} has won the game`;
+    score2track++
+    scorep2.textContent = score2track;
 }
-
-    
 };
+
+playAgain.addEventListener('click',()=>{
+
+    winnerScreen.classList.remove('active');
+
+    for (let i=0;i<=game.markers.length;i++) {
+       if (game.markers[i]==='x' || game.markers[i]==='o') {
+           game.markers.splice(i,1,'');
+       }
+    }
+    game.cells.forEach(cell =>{
+        cell.classList.remove('x');
+        cell.classList.remove('o');
+    })
+    game.playGame();
+});
+
+
+reset.addEventListener('click', ()=>{
+
+
+})
 
 return {endGame}
 })();
